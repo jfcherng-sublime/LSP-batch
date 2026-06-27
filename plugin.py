1c8459caa604af8af754b3903c24753e73a7e0d5
Jack Cherng <jfcherng@gmail.com>
2026-06-27 14:11:13 +0800

    refactor(plugin): migrate from NpmClientHandler to LspPlugin + NodeManager
    Switch to the newer lsp_utils API that separates node management from the
    plugin lifecycle. Update settings to use the new flat command/selector/schemes
    format. Add typing-extensions dependency for runtime override support.
    
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin.py
+++ plugin.py
@@ -1,16 +1,35 @@
-import os
-from lsp_utils import NpmClientHandler
+from __future__ import annotations
+
+from pathlib import Path
+from typing import final
+
+from LSP.plugin import LspPlugin
+from LSP.plugin import OnPreStartContext
+from lsp_utils import NodeManager
+from sublime_lib import ResourcePath
+from typing_extensions import override
 
 
-def plugin_loaded():
-    LspBatchPlugin.setup()
+@final
+class LspBatchPlugin(LspPlugin):
+    @classmethod
+    @override
+    def on_pre_start_async(cls, context: OnPreStartContext) -> None:
+        package_name = cls.plugin_storage_path.name
+        NodeManager.on_pre_start_async(
+            context,
+            cls.plugin_storage_path,
+            ResourcePath("Packages", package_name, "language-server"),
+            Path("out", "server.js"),
+            node_version_requirement=">=18",
+        )
 
 
-def plugin_unloaded():
-    LspBatchPlugin.cleanup()
+def plugin_loaded() -> None:
+    """Executed when this plugin is loaded."""
+    LspBatchPlugin.register()
 
 
-class LspBatchPlugin(NpmClientHandler):
-    package_name = __package__
-    server_directory = "language-server"
-    server_binary_path = os.path.join(server_directory, "out", "server.js")
+def plugin_unloaded() -> None:
+    """Executed when this plugin is unloaded."""
+    LspBatchPlugin.unregister()
